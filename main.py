e17b8ec9c9d030e02fd13b627ce9f3ca2459465d
Lohi712 <lohithkalepu@gmail.com>
2025-09-03 18:25:14 +0530

    Spotify Control - Incomplete

--- main.py
+++ main.py
@@ -2,8 +2,16 @@
 import time
 import win32com.client
 import datetime
+import wikipedia
 import speech_recognition as sr
+import webbrowser
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+SPOTIFY_CLIENT_ID = 'e8962aec09aa4d24aace46f0bf3a0f0a'
+SPOTIFY_CLIENT_SECRET = 'd710ee25cb3947f0a85708170b3496ff'
+SPOTIFY_REDIRECT_URL = 'http://127.0.0.1:8888/callback'
 # Function to speak
 def speak(audio):
     speaker = win32com.client.Dispatch("SAPI.SpVoice")  # "SAPI.SpVoice" is the official name of the Microsoft Speech API voice feature.
@@ -37,5 +45,54 @@ def listen():
 
 # main function
 if __name__ == "__main__":
+    
     wish()
-    listen()
+    while True:
+        query = listen().lower()
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia...")
+            query = query.replace("wikipedia","")
+            Wikiresults = wikipedia.summary(query,sentences = 2)
+            speak("According to Wikipedia..")
+            print(Wikiresults)
+            speak(Wikiresults)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        
+        elif 'open github' in query:
+            webbrowser.open('github.com')
+
+        elif 'the time' in query:
+            Time = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Currently it is {Time}")
+
+        elif 'open code' in query:
+            codePath = r"C:\Users\USER\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+            os.startfile(codePath)
+        
+        elif 'on spotify' in query:
+            song = query.replace('on spotify','').replace('play','').strip()
+            if song:
+                try:
+                    scope = "user-modify-playback-state user-read-playback-state"
+                    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,client_secret=SPOTIFY_CLIENT_SECRET,redirect_uri=SPOTIFY_REDIRECT_URL,scope=scope))
+                    Music_results = sp.search(q=song,limit=1)
+                    if not Music_results['tracks']['items']:
+                        speak(f"Sorry, I could not find the song {song} on spotify.")
+                    else:
+                        track = Music_results['tracks']['items'][0]
+                        track_uri = track['uri']
+                        sp.start_playback(uris=[track_uri])
+                        speak(f"Playing {track['name']} by {track['artists'][0]['name']} on Spotify")
+                except Exception as e:
+                    speak("Could not connect to Spotify. Please make sure it is opened before this code started running")
+                    print(f"Error: {e}")
+            else:
+                speak("What song would u like to play on Spotify?")
+
+            speak(f"Searching for {song} on Spotify")
+            webbrowser.open(f"https://open.spotify.com/search/SONG%20NAME")
